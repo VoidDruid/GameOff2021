775f7991ca25a148b5634d270120f072194308e0
Kovalev Vasiliy <minskij10@yandex.ru>
2021-11-28 22:27:39 +0300

    Grant tab

--- objects/ui/GrantTab.gd
+++ objects/ui/GrantTab.gd
@@ -13,6 +13,12 @@ func _on_Button_pressed():
 
 var grant_left_tab_color_path = "HBoxContainer/LeftTabColor"
 var grant_texture_color_path = "HBoxContainer/BackgroundColor"
+var grant_add_info_path = "HBoxContainer/Background/HBoxContainer/Control"
 
-func setup_for_grant(grant, left_tab_color, EffectLabel):
+func setup_for_grant(grant, left_tab_color, EffectLabel, PlusButton, GrantChance, is_available, is_current, is_completed):
     get_node(grant_left_tab_color_path).color = left_tab_color
+    if is_available:
+        get_node(grant_add_info_path).add_child(PlusButton.instance())
+    elif is_current:
+        get_node(grant_add_info_path).add_child(GrantChance.instance())
+
